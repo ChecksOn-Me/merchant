8c04dcf9735514d3d70cd00ead6c847bc2c7345a
slowgen <kalibobailey@gmail.com>
2021-03-14 16:17:52 -0400

    Feat: Autoscrolling to show login fields and input button

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -39,68 +39,61 @@ class _LoginScreenState extends State<LoginScreen> {
           style: kTextStyleLightPink,
         ),
       ),
-      body: SingleChildScrollView(
-        child: Container(
-          color: kDarkBackgroundColor,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Image.asset(
-                'assets/images/beerpour300sq.gif',
-                fit: BoxFit.contain,
-              ),
-              Text(
-                'Please wait... pouring in progress',
-                textAlign: TextAlign.center,
-                style: kTextStyleLightPink,
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  textAlign: TextAlign.center,
-                  style: kTextStyleDark,
-                  decoration: kTextFieldInputDecoration.copyWith(
-                    hintText: 'email',
-                    icon:
-                        Icon(Icons.login_rounded, color: kLightAccentColorPink),
-                  ),
-                  onChanged: (value) {
-                    email = value;
-                  },
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  obscureText: true,
-                  textAlign: TextAlign.center,
-                  style: kTextStyleDark,
-                  decoration: kTextFieldInputDecoration.copyWith(
-                    hintText: 'password',
-                    icon:
-                        Icon(Icons.lock_rounded, color: kLightAccentColorPink),
-                  ),
-                  onChanged: (value) {
-                    password = value;
-                  },
-                ),
-              ),
-              InputButton(
-                text: 'Login or Register',
-                onPressed: () {
-                  final session = LoginOrRegister(
-                    email: email,
-                    password: password,
-                    context: context,
-                  );
-                  session.login();
-                  print('last step');
-                },
-              ),
-            ],
+      backgroundColor: kDarkBackgroundColor,
+      body: ListView(
+        reverse: true,
+        children: [
+          Image.asset(
+            'assets/images/beerpour300sq.gif',
+            fit: BoxFit.contain,
           ),
-        ),
+          Text(
+            'Please wait... pouring in progress',
+            textAlign: TextAlign.center,
+            style: kTextStyleLightPink,
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              textAlign: TextAlign.center,
+              style: kTextStyleDark,
+              decoration: kTextFieldInputDecoration.copyWith(
+                hintText: 'email',
+                icon: Icon(Icons.login_rounded, color: kLightAccentColorPink),
+              ),
+              onChanged: (value) {
+                email = value;
+              },
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: TextField(
+              obscureText: true,
+              textAlign: TextAlign.center,
+              style: kTextStyleDark,
+              decoration: kTextFieldInputDecoration.copyWith(
+                hintText: 'password',
+                icon: Icon(Icons.lock_rounded, color: kLightAccentColorPink),
+              ),
+              onChanged: (value) {
+                password = value;
+              },
+            ),
+          ),
+          InputButton(
+            text: 'Login or Register',
+            onPressed: () {
+              final session = LoginOrRegister(
+                email: email,
+                password: password,
+                context: context,
+              );
+              session.login();
+              print('last step');
+            },
+          ),
+        ].reversed.toList(),
       ),
     );
   }
